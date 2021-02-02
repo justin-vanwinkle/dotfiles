a59e5a6c485c5ba9a751f9d0a01e5a1c9a867727
Justin VanWinkle <vanwinkle.justin@gmail.com>
2021-02-01 23:14:54 -0500

    add echo output to vundle install script

--- vim/install.sh
+++ vim/install.sh
@@ -1,5 +1,9 @@
+echo "Checking for Vundle..."
 if test -d ~/.vim/bundle
 then
+    echo "Vundle found"
+else
+    echo "Vundle not found. Installing..."
     mkdir -p ~/.vim/bundle
     pushd ~/.vim/bundle
     git clone https://github.com/VundleVim/Vundle.vim.git
